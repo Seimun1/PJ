bf96af7114a7d6cba874cbd0f7086e7366d539b8
AlexNikitina <alex.nikitina82@gmail.com>
2023-06-08 22:51:27 +0500

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,18 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+
+
+
+
+
+
+
+
+
+}
